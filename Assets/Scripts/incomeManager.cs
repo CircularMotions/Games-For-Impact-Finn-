12d543f7a61b19a0c4a359a5473baa68f953c3de
DmitryY <D23125246@mytudublin.ie>
2026-03-01 13:37:46 +0000

    Passive income created

---
+++ Assets/Scripts/incomeManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Timers;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
+public class incomeManager : MonoBehaviour
+{
+    private int _coffeeIncome = 1;
+    public TextMeshProUGUI BalanceText;
+    private int balance;
+    private float timer = 7f;
+
+    private void Update()
+    {
+        BalanceText.text = $"Balance: {balance}";
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (timer <= 0f)
+        {
+            balance += _coffeeIncome;
+            timer = 7f;
+        }
+    }
+
+    // private IEnumerator CoffeIncome()
+    // {
+    //     timer = 7;
+    //     yield return new WaitForSeconds(7);
+    //     balance += _coffeeIncome;
+    // }
+}
