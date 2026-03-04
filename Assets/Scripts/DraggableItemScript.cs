2abc4753647fc5103648d4e81041e302071176c8
CircularMotions <C23328021@mytudublin.ie>
2026-03-04 18:43:08 +0000

    Added basic workshop UI
    Temporary button to allow switching between cafe and workshop. Finish button has no function. Draggable tools at the bottom reset to position and have no function yet.

---
+++ Assets/Scripts/DraggableItemScript.cs
@@ -0,0 +1,25 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableItemScript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Vector3 startTransform;
+    void Awake()
+    {
+        startTransform = transform.position;
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = startTransform;
+    }
+}
