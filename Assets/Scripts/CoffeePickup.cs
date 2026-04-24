23b89102891896273ee8139c05a7a9af47516f93
DmitryY <D23125246@mytudublin.ie>
2026-04-24 15:53:53 +0100

    Randomised coffee suplies decrease

--- Assets/Scripts/CoffeePickup.cs
+++ Assets/Scripts/CoffeePickup.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class CoffeePickup : MonoBehaviour
 {
     public GameObject InventoryScript;
     public GameObject nextTarget;
     [SerializeField] private ParticleSystem particle;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Customer"))
@@ -40,12 +44,23 @@ public class CoffeePickup : MonoBehaviour
     public void coffeeIncome()
     {
         gameObject.GetComponent<AudioSource>().Play();
-        // decrease number of coffee beans
-        InventoryScript.GetComponent<InventoryManager>().itemList.item4Quantity--;
-        InventoryScript.GetComponent<InventoryManager>().itemList.item5Quantity--;
-        InventoryScript.GetComponent<InventoryManager>().itemList.item6Quantity--;
-        InventoryScript.GetComponent<InventoryManager>().itemList.item7Quantity--;
-        InventoryScript.GetComponent<InventoryManager>().itemList.item8Quantity--;
+        
+        List<Action> items = new List<Action>()
+        {
+            () => InventoryScript.GetComponent<InventoryManager>().itemList.item4Quantity--,
+            () => InventoryScript.GetComponent<InventoryManager>().itemList.item5Quantity--,
+            () => InventoryScript.GetComponent<InventoryManager>().itemList.item6Quantity--,
+            () => InventoryScript.GetComponent<InventoryManager>().itemList.item7Quantity--,
+            () => InventoryScript.GetComponent<InventoryManager>().itemList.item8Quantity--
+        };
+
+
+        int count = Random.Range(2, 4);
+        
+        items = items.OrderBy(x => Random.value).ToList();
+
+        for (int i = 0; i < count; i++)
+            items[i]();
         // add balance
         InventoryScript.GetComponent<InventoryManager>().itemList.balance += 5;
         //etc.
